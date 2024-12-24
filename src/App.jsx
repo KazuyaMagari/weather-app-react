efa539ea829aaa221b71e00b99e1f16bf2e28701
KazuyaMagari <kazuyamagarifuchi1211@gmail.com>
2024-12-23 19:08:11 -0500

    styled component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,17 @@ import Title from "./components/Title"
 import Form from "./components/Form"
 import Result from "./components/Result"
 import {useState} from "react"
+import { createGlobalStyle } from 'styled-components'
+import IMG from './assets/flat-design-monsoon-season-clouds-illustration_23-2149424294.jpeg'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-image: url(${IMG});
+    background-size: cover;
+    background-repeat: no-repeat;
+    margin: 0;
+  }
+`
 const App = () =>{
   const [city, setCity] =useState("")
   const [results, setResults] = useState({
@@ -13,7 +24,8 @@ const App = () =>{
   })
   const getWeather = (e) => {
         e.preventDefault()
-        fetch(`http://api.weatherapi.com/v1/current.json?key=991c75caef90400a86812232242210&q=${city}&aqi=no`)
+        fetch(`
+http://api.weatherapi.com/v1/current.json?key=1056435816a1487ab09224739242312&q=${city}&aqi=no`)
         .then(res => res.json())
         .then(data => {
           setResults({
@@ -27,6 +39,7 @@ const App = () =>{
     }
   return (
   <div>
+    <GlobalStyle/>
     <Title/>
     <Form setCity={setCity} getWeather={getWeather}/>
 
