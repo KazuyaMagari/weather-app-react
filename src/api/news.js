04c8f8a9027077b53d4bee6f843e3b8c25222f62
KazuyaMagari <kazuyamagarifuchi1211@gmail.com>
2025-03-20 12:23:29 -0400

    change card style

--- src/api/news.js
+++ src/api/news.js
@@ -2,7 +2,7 @@ export const fetchNews = async (startDate) => {
     const API_KEY = "a8a03c38430945e9a7ab0ede1ad208e5";
     const BASE_URL = "https://newsapi.org/v2/everything";
     
-    const url = `${BASE_URL}?q=(Google OR Amazon) AND Cloud&from=${startDate}T00:00:00&to=${startDate}T23:59:59&sortBy=relevancy&pageSize=5&apiKey=${API_KEY}`;
+    const url = `${BASE_URL}?q=(Google OR Amazon) AND Cloud&from=${startDate}T00:00:00&to=${startDate}T23:59:59&sortBy=relevancy&pageSize=8&apiKey=${API_KEY}`;
     
     try {
         const response = await fetch(url);
--- src/components/NewsComp.jsx
+++ src/components/NewsComp.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
 import { fetchNews } from '../api/news';  
 
 function NewsComp() {
@@ -18,50 +19,52 @@ function NewsComp() {
   };
 
   return (
-    <>
-      <div className='container mt-5'>
-        <div className="row justify-content-center">
-          <div className="col-5">
-            <h2 className=''>Know the news and weather at the same time (due to the setting of API, it works on only localhost)</h2>
-            <Form className='align-items-center mb-4' onSubmit={handleSubmit}>
-              <Form.Group controlId="formDate">
-                <Form.Label>Select a date (invalid a month ago from today) :</Form.Label>
-                <Form.Control 
-                  type="date" 
-                  value={startDate} 
-                  onChange={(e) => setStartDate(e.target.value)}
-                  className='me-2 mb-3'
-                />
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
-            </Form>
-          </div>  
-        </div> 
-      </div>
-      
-      <div className='container'>
-        <div className="row align-items-center">
-          {news.length > 0 ? (
-            news.map((article, index) => (
-              <div key={index} className="card mb-3 col-6 justify-content-center">
-                <img src={article.urlToImage} alt="News" className="card-img-top" />
-                <div className="card-body">
-                  <h5 className="card-title">{article.title}</h5>
-                  <p className="card-text">{article.description}</p>
-                  <a href={article.url} className="btn btn-primary" target="_blank" rel="noopener noreferrer">
-                    記事を読む
-                  </a>
-                </div>
-              </div>
-            ))
-          ) : (
-            <p className="text-center">ニュースがありません。</p>
-          )}
+      <>
+        <div className="container mt-5">
+          <div className="row justify-content-center">
+            <div className="col-5">
+              <h2>Know the news and weather at the same time (due to the setting of API, it works on only localhost)</h2>
+              <Form className="align-items-center mb-4" onSubmit={handleSubmit}>
+                <Form.Group controlId="formDate">
+                  <Form.Label>Select a date (invalid a month ago from today):</Form.Label>
+                  <Form.Control 
+                    type="date" 
+                    value={startDate} 
+                    onChange={(e) => setStartDate(e.target.value)}
+                    className="me-2 mb-3"
+                  />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+              </Form>
+            </div>  
+          </div> 
         </div>
-      </div>
-    </>
+    
+        <div className="container">
+          <div className="row align-items-center">
+            {news.length > 0 ? (
+              news.map((article, index) => (
+                <div className="col-md-4 mb-4" key={index} style={{ display: "flex", justifyContent: "center" , padding: "0", margin: "0", height: "600px", width: "300px"}}>
+                  <Card style={{ width: "18rem" }}>
+                    <Card.Img variant="top" src={article.urlToImage} alt="News" style={{ height: "200px", objectFit: "cover" }}/>
+                    <Card.Body style={{ height: "350px" }}>
+                      <Card.Title>{article.title}</Card.Title>
+                      <Card.Text>{article.description}</Card.Text>
+                      <a href={article.url} className="btn btn-primary" target="_blank" rel="noopener noreferrer">
+                        Read Article
+                      </a>
+                    </Card.Body>
+                  </Card>
+                </div>
+              ))
+            ) : (
+              <p className="text-center">ニュースがありません。</p>
+            )}
+          </div>
+        </div>
+      </>
   );
 }
 
